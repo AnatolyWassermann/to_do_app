bd60643ce71ceb0b06621ccd3f43eec7b959fc12
Anatoly <seruderu@gmail.com>
2023-03-06 22:34:36 +0300

    minor html/css tweaks

--- routers.py
+++ routers.py
@@ -1,17 +1,17 @@
 '''obsolete'''
 
-class ToDoDBRouter:
-    def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'ToDo':
-            return 'mongo'
-        return None
+# class ToDoDBRouter:
+#     def db_for_read(self, model, **hints):
+#         if model._meta.app_label == 'ToDo':
+#             return 'mongo'
+#         return None
 
-    def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'ToDo':
-            return 'mongo'
-        return None
+#     def db_for_write(self, model, **hints):
+#         if model._meta.app_label == 'ToDo':
+#             return 'mongo'
+#         return None
 
-    def allow_relation(self, obj1, obj2, **hints):
-        if obj1._meta.app_label == 'ToDo' or obj2._meta.app_label == 'ToDo':
-            return True
-        return None
+#     def allow_relation(self, obj1, obj2, **hints):
+#         if obj1._meta.app_label == 'ToDo' or obj2._meta.app_label == 'ToDo':
+#             return True
+#         return None
