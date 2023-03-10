99fc1fa9888d8fb405c969bdb0b6ece67f7e2f23
Serdar Camur <99121106+AnatolyWassermann@users.noreply.github.com>
2023-03-10 18:38:17 +0300

    Update models.py

--- Todolist/models.py
+++ Todolist/models.py
@@ -1,14 +1,12 @@
 
 import datetime
 import random
-import unicodedata
-import string
-import re
 from mongoengine import (Document, 
                          StringField, 
                          BooleanField, 
                          DateTimeField,
                          IntField)
+import django.utils.text import slugify
 
 def random_string_generator(size=5, chars = string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
@@ -28,17 +26,9 @@ class ToDo(Document):
         return self.title
     
     def save(self, *args, **kwargs):
-        ''' since slugify function doesn't support mongoengine i customized it a bit '''
+        
         if not self.slug:
-            # self.slug = self.title.replace(' ', '-') + random_slug_generator(size=10)
-            current_title = str(self.title)
-            current_title = (
-                unicodedata.normalize("NFKD", current_title)
-                .encode('ascii', 'ignore')
-                .decode('ascii')
-            )
-            current_title = re.sub(r"[^\w\s-]", "", current_title.lower())
-            current_title = re.sub(r"[-\s]+", "-", current_title).strip("-_")
-            self.slug = current_title + random_string_generator(size=5)
+            self.slug = slugify(self.title) + random_string_generator(size=5)
+            
 
         return super(ToDo, self).save(*args, **kwargs)
