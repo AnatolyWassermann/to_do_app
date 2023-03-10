98b66f170567e36d6bee34f6942e0b91bbf6b2e6
Serdar Camur <99121106+AnatolyWassermann@users.noreply.github.com>
2023-03-10 18:38:44 +0300

    Update models.py

--- Todolist/models.py
+++ Todolist/models.py
@@ -7,6 +7,7 @@ from mongoengine import (Document,
                          DateTimeField,
                          IntField)
 import django.utils.text import slugify
+import string
 
 def random_string_generator(size=5, chars = string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
